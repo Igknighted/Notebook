7d9083f40e3e046275f7b02386632664ec1c6aa2
Sam <sam.igknighted@gmail.com>
2014-06-21 23:40:13 -0400

    chrooting method/ it has bugs

--- CoreOS/access_methods/enter_container.sh
+++ CoreOS/access_methods/enter_container.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo Problems will exist when trying to inspect processes.
+echo Problems will exist when trying to inspect processes.
+echo Problems will exist when trying to inspect processes.
 
 dir=$(docker inspect nimble | awk -F'"' '{if($2 == "ID") print $4}')
 imgpath="/var/lib/docker/btrfs/subvolumes/$dir"
