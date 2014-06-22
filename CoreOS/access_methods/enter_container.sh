48762b81b1ebe5c2dc4cd8a90bf4045d15b86172
Sam <sam.igknighted@gmail.com>
2014-06-21 23:35:24 -0400

    Update and rename docker_chroot.sh to enter_container.sh

--- CoreOS/access_methods/enter_container.sh
+++ CoreOS/access_methods/enter_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo Problems will exist when trying to inspect processes.
 
 dir=$(docker inspect nimble | awk -F'"' '{if($2 == "ID") print $4}')
 imgpath="/var/lib/docker/btrfs/subvolumes/$dir"
@@ -7,4 +8,3 @@ imgpath="/var/lib/docker/btrfs/subvolumes/$dir"
 if [ -d "$imgpath" ]; then
 	chroot $imgpath
 fi
-exit
