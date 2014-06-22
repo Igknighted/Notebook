c406242820601b610aeb87bb6197e89b72613030
Sam <sam.igknighted@gmail.com>
2014-06-22 00:47:02 -0400

    Update container.sh

--- CoreOS/access_methods/container.sh
+++ CoreOS/access_methods/container.sh
@@ -13,7 +13,7 @@ if [ ! -f ~/.sshto/key ]; then
 	ssh-keygen -t rsa -N "" -f ~/.sshto/key
 fi
 
-dir=$(docker inspect nimble | awk -F'"' '{if($2 == "ID") print $4}')
+dir=$(docker inspect $1 | awk -F'"' '{if($2 == "ID") print $4}')
 imgpath="/var/lib/docker/btrfs/subvolumes/$dir"
 
 if [ -d "$imgpath" ]; then
