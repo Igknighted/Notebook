22885e7fe8be62e6035f67a77bc80601c5411b5d
Sam <sam.igknighted@gmail.com>
2015-04-03 11:21:01 -0400

    Update lazy_ssh.pl

--- Automation/lazy_ssh.pl
+++ Automation/lazy_ssh.pl
@@ -2,7 +2,7 @@
 use Expect;
 
 
-sub run {
+sub sh {
 	$cmd = $_[0];
 	$exp->expect(undef, [$prompt => sub { $exp->send("$cmd\n"); } ]);
 }
@@ -19,10 +19,12 @@ $user = $ARGV[0];
 $host = $ARGV[1];
 $pw   = $ARGV[2];
 $dir  = $ARGV[3];
+
 # Setup our config variables
 $prompt = "_# ";
-$exp = new Expect();
 
+
+$exp = new Expect();
 # Some debugging stuff
 #$exp->raw_pty(1);  
 #$exp->log_stdout(0);
@@ -33,17 +35,20 @@ $exp->spawn("ssh -l $user $host -o UserKnownHostsFile=/dev/null -o StrictHostKey
 # Login to the server
 $exp->expect(undef, ["[Pp]assword:" => sub { $exp->send("$pw\n"); } ]);
 # Set the prompt to something we can trust
-$exp->expect(undef, ["[#\$]" => sub { $exp->send("PS1='$prompt'\n"); } ]);
+$exp->expect(undef, ["[#\$]" => sub { $exp->send("PS1='\$?$prompt';\n"); } ]);
 
+sh("mkdir -p ~/$dir");
+sh("cd ~/$dir");
+sh("touch testing12345");
+sh("thisfails");
 
-run("mkdir -p ~/$dir");
-run("cd ~/$dir");
-run("touch testing12345");
-run("exit");
+sh("date");
+sh("exit");
+
 
 
 # If we want to provide an interactive shell, we can...
 #$exp->interact();
 
 # Doing a soft_close makes expect wait patiently for the EOF
-$exp->soft_close();   
+$exp->soft_close();
