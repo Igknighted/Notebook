d0d5e32231a92a47dba209a1fcceee24ae40d2f2
Sam <sam.igknighted@gmail.com>
2017-09-28 15:43:53 -0500

    Update terminal.sh

--- WindowsSubsystemLinux/terminal.sh
+++ WindowsSubsystemLinux/terminal.sh
@@ -6,7 +6,7 @@ gnome-terminal
 # We need to hold the first bash command open for WSL to act right.
 if [[ $(ps faux | grep bash | head -n1 | awk '{print $2}') -eq $$ ]]; then
 	while true; do
-		sleep 1s
+		sleep 600s
 		if [[ $(ps --ppid `ps aux | grep gnome-terminal-server | awk '{print $2}'` | grep -v PID) == "" ]]; then
 			exit 0
 		fi
