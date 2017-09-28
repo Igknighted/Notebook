9471dbd417eeaaab5a3304a575a30ccee12d59bc
Sam <sam.igknighted@gmail.com>
2017-09-28 15:40:18 -0500

    Create terminal.sh

---
+++ WindowsSubsystemLinux/terminal.sh
@@ -0,0 +1,14 @@
+cd ~
+export DISPLAY=:0.0 
+gnome-terminal
+
+
+# We need to hold the first bash command open for WSL to act right.
+if [[ $(ps faux | grep bash | head -n1 | awk '{print $2}') -eq $$ ]]; then
+	while true; do
+		sleep 1s
+		if [[ $(ps --ppid `ps aux | grep gnome-terminal-server | awk '{print $2}'` | grep -v PID) == "" ]]; then
+			exit 0
+		fi
+	done
+fi
